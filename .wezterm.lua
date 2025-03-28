ae54f1a183bbbf3fbbb7608f40c9cfcae469305f
Kai Chevannes <chevannes.kai@gmail.com>
2025-03-28 10:28:49 +0000

    feat: basic config

---
+++ .wezterm.lua
@@ -0,0 +1,57 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+-- shell
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    config.default_domain = 'WSL:Ubuntu'
+end
+ 
+-- beeps
+config.audible_bell = "Disabled"
+
+-- tabs
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+
+-- colors
+-- config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
+config.color_scheme = 'Catppuccin Mocha'
+-- config.color_scheme = "SleepyHollow"
+-- config.color_scheme = "Gruvbox Dark (Gogh)"
+
+-- font
+config.font = wezterm.font "Dank Mono"
+config.font_size = 14.0;
+
+-- keybindings
+-- config.disable_default_key_bindings = true
+local act = wezterm.action
+config.leader = {key='w', mods='ALT', timeout_milliseconds=1000}
+
+config.keys = {
+    { key = '1', mods = 'ALT', action = act.ActivateTab(0)},
+    { key = '2', mods = 'ALT', action = act.ActivateTab(1)},
+    { key = '3', mods = 'ALT', action = act.ActivateTab(2)},
+    { key = '4', mods = 'ALT', action = act.ActivateTab(3)},
+    { key = '5', mods = 'ALT', action = act.ActivateTab(4)},
+    { key = '6', mods = 'ALT', action = act.ActivateTab(5)},
+    { key = '7', mods = 'ALT', action = act.ActivateTab(6)},
+    { key = '8', mods = 'ALT', action = act.ActivateTab(7)}, 
+    { key = '9', mods = 'ALT', action = act.ActivateTab(8)},
+    { key = 't', mods = 'ALT', action = act.SpawnTab 'CurrentPaneDomain'},
+    { key = 'q', mods = 'ALT', action = act.CloseCurrentTab{confirm = false}},
+    { key = 'c', mods = 'LEADER', action = act.CloseCurrentPane{confirm = false}},
+    { key = 't', mods = 'LEADER', action = act.SpawnTab 'CurrentPaneDomain'},
+    { key = 'v', mods = 'LEADER', action = act.SplitHorizontal{domain = 'CurrentPaneDomain'}},
+    { key = 's', mods = 'LEADER', action = act.SplitVertical{domain = 'CurrentPaneDomain'}},
+    { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection 'Left'},
+    { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection 'Down'},
+    { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection 'Up'},
+    { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection 'Right'},
+}
+
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ .zshrc
@@ -0,0 +1,43 @@
+precmd() {
+  echo -ne "\033]7;file://$PWD\033\\"
+}
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+bindkey -v
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/cheva/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+# Download plugins
+# To update: rm -rf ~/.zsh && exec zsh
+if [[ ! -e ~/.zsh/powerlevel10k ]]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
+fi
+if [[ ! -e ~/.zsh/zsh-syntax-highlighting ]]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+fi
+if [[ ! -e ~/.zsh/zsh-autosuggestions ]]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+fi
+
+# Source plugins
+source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
