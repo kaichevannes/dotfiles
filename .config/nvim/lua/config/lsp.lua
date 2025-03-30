e3929ef8fd88a2e9e69ac6f3181ffe080d752386
Kai Chevannes <chevannes.kai@gmail.com>
2025-03-30 16:05:37 +0100

    refactor: remove the autocommand group code

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -25,7 +25,6 @@ vim.diagnostic.config({
 vim.o.completeopt = "menuone,noinsert,popup,fuzzy"
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("my.lsp", {}),
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
@@ -38,7 +37,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     if not client:supports_method("textDocument/willSaveWaitUntil")
         and client:supports_method("textDocument/formatting") then
       vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
         buffer = args.buf,
         callback = function()
           vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
