a9d4064a13e398d798f3fbfeaf34f521368c95c5
Kai Chevannes <chevannes.kai@gmail.com>
2025-05-21 15:08:47 +0100

    feat: switch to nvim dap view

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -2,80 +2,14 @@ return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
+		"igorlfs/nvim-dap-view",
 		"theHamsta/nvim-dap-virtual-text",
 		"nvim-neotest/nvim-nio",
 		"williamboman/mason.nvim",
 		"jay-babu/mason-nvim-dap.nvim",
 	},
-	keys = {
-		{
-			"<F5>",
-			function()
-				require("dap").continue()
-			end,
-			desc = "Debug: Start/Continue",
-		},
-		{
-			"<F3>",
-			function()
-				require("dap").step_back()
-			end,
-		},
-		{
-			"<F4>",
-			function()
-				require("dap").step_out()
-			end,
-			desc = "Debug: Step Over",
-		},
-		{
-			"<F7>",
-			function()
-				require("dap").step_into()
-			end,
-			desc = "Debug: Step Into",
-		},
-		{
-			"<F8>",
-			function()
-				require("dap").step_over()
-			end,
-			desc = "Debug: Step Out",
-		},
-		{
-			"<leader>b",
-			function()
-				require("dap").toggle_breakpoint()
-			end,
-			desc = "Debug: Toggle Breakpoint",
-		},
-		{
-			"<leader>B",
-			function()
-				require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-			end,
-			desc = "Debug: Set Breakpoint",
-		},
-		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-		{
-			"<F6>",
-			function()
-				require("dapui").toggle()
-			end,
-			desc = "Debug: See last session result.",
-		},
-		{
-			"g=",
-			function()
-				require("dapui").eval(nil, { enter = true })
-			end,
-			desc = "Evaluate under cursor",
-		},
-	},
 	config = function()
 		require("nvim-dap-virtual-text").setup()
-		local dap = require("dap")
-		local dapui = require("dapui")
 
 		require("mason-nvim-dap").setup({
 			-- Makes a best effort to setup the various debuggers with
@@ -89,12 +23,20 @@ return {
 			ensure_installed = { "js" },
 		})
 
-		dapui.setup()
+		local dap = require("dap")
 
-		-- dap.listeners.before.attach["dapui_config"] = dapui.open
-		-- dap.listeners.before.launch["dapui_config"] = dapui.open
-		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-		dap.listeners.before.event_exited["dapui_config"] = dapui.close
+		-- local dapui = require("dapui")
+		--
+		-- dapui.setup()
+		--
+		-- dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+		-- dap.listeners.before.event_exited["dapui_config"] = dapui.close
+
+		local dapview = require("dap-view")
+		dap.listeners.before.attach["dap-view-config"] = dapview.open
+		dap.listeners.before.launch["dap-view-config"] = dapview.open
+		-- dap.listeners.before.event_terminated["dap-view-config"] = dapview.close
+		-- dap.listeners.before.event_exited["dap-view-config"] = dapview.close
 
 		-- Javascript / Typescript
 		-- Example: https://github.com/ecosse3/nvim/blob/13f1d35ca2fe326f3f4cdd516ae577eb1cef0921/lua/plugins/dap.lua
@@ -127,5 +69,21 @@ return {
 				skipFiles = { "<node_internals>/**", "node_modules/**" },
 			},
 		}
+
+		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+		vim.keymap.set("n", "<F6>", "<CMD>DapViewToggle!<CR>", { desc = "Debug: See last session result (dapview)" })
+		-- vim.keymap.set("n", "<F6>", dapui.toggle, { desc = "Debug: See last session result (dapui)" })
+
+		vim.keymap.set("n", "<F3>", dap.step_back, { desc = "Debug: Step Back" })
+		vim.keymap.set("n", "<F4>", dap.step_out, { desc = "Debug: Step Out" })
+		vim.keymap.set("n", "<F7>", dap.step_into, { desc = "Debug: Step Into" })
+		vim.keymap.set("n", "<F8>", dap.step_over, { desc = "Debug: Step Over" })
+
+		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+		vim.keymap.set("n", "<leader>B", function()
+			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+		end, { desc = "Debug: Conditional Breakpoint" })
+
+		vim.keymap.set("n", "g=", require("dap.ui.widgets").hover, { desc = "Debug: Evaluate expression" })
 	end,
 }
