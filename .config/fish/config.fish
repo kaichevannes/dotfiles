f30fa634a338f1acf00550530fbba8d66777a122
Kai Chevannes <chevannes.kai@gmail.com>
2025-10-06 12:36:24 +0100

    feat: helix config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,26 +1,26 @@
 if status is-interactive
-  set fish_greeting
+    set fish_greeting
 
-  # Y to accept
-  bind \cy accept-autosuggestion
+    # Y to accept
+    bind \cy accept-autosuggestion
 
-  # Initialise homebrew
-  set -x PATH "/opt/homebrew/bin:/home/linuxbrew/.linuxbrew/bin:/usr/local/bin:$PATH"
-  eval (brew shellenv)
+    # Initialise homebrew
+    set -x PATH "/opt/homebrew/bin:/home/linuxbrew/.linuxbrew/bin:/usr/local/bin:$PATH"
+    eval (brew shellenv)
 
-  set -x VISUAL nvim
-  set -x EDITOR nvim
+    set -x VISUAL hx
+    set -x EDITOR hx
 
-  # Start ssh agent
-  # keychain --quiet --eval id_ed25519 | source
-  # Only start ssh-agent if not already running
-  if not set -q SSH_AUTH_SOCK
-      eval (ssh-agent -c) > /dev/null
-      ssh-add -q ~/.ssh/id_ed25519
-  end
+    # Start ssh agent
+    # keychain --quiet --eval id_ed25519 | source
+    # Only start ssh-agent if not already running
+    if not set -q SSH_AUTH_SOCK
+        eval (ssh-agent -c) >/dev/null
+        ssh-add -q ~/.ssh/id_ed25519
+    end
 
-  starship init fish | source
-  enable_transience
+    starship init fish | source
+    enable_transience
 end
 
 # bun
@@ -29,3 +29,7 @@ set --export PATH $BUN_INSTALL/bin $PATH
 
 # zettel
 set -x ZK_NOTEBOOK_DIR ~/zettel
+
+# dotnet
+set -x DOTNET_ROOT $HOME/.dotnet
+set -x PATH $PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
