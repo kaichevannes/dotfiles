96e526bc382add334bd699908a6a1ab6db2ff98c
Kai Chevannes <kai.chevannes@goteleport.com>
2026-07-16 20:48:07 +0100

    feat: 2026 config using stow

---
+++ Brewfile
@@ -0,0 +1,22 @@
+tap "nikitabobko/tap"
+
+brew "ripgrep"
+brew "fzf"
+brew "zoxide"
+brew "tmux"
+brew "yazi"
+brew "lazygit"
+brew "stow"
+
+brew "lnav"
+brew "kubernetes-cli"
+brew "kubectx"
+brew "helm"
+
+brew "neovim"
+brew "tree-sitter"
+brew "tree-sitter-cli"
+brew "yaml-language-server"
+brew "lua-language-server"
+
+cask "nikitabobko/tap/aerospace", trusted: true
---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,90 @@
+local servers = { "lua_ls", "yamlls" }
+local parsers = { "lua", "yaml" }
+
+vim.g.mapleader = " "
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.undofile = true
+vim.o.scrolloff = 8
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.signcolumn = "yes"
+vim.o.updatetime = 250
+vim.o.termguicolors = true
+
+vim.api.nvim_create_autocmd('PackChanged', {
+	callback = function(ev)
+		local name, kind = ev.data.spec.name, ev.data.kind
+		if name == 'telescope-fzf-native.nvim' and (kind == 'install' or kind == 'update') then
+			vim.system({ 'make' }, { cwd = ev.data.path })
+		end
+	end,
+})
+
+vim.pack.add({
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/folke/lazydev.nvim",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/sainnhe/gruvbox-material",
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/nvim-telescope/telescope.nvim",
+  "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
+  "https://github.com/mikavilpas/yazi.nvim",
+  "https://github.com/lewis6991/gitsigns.nvim",
+  "https://github.com/tpope/vim-surround",
+  "https://github.com/tpope/vim-repeat"
+})
+
+vim.lsp.enable(servers)
+
+vim.diagnostic.config({ virtual_text = true })
+
+require("lazydev").setup()
+require("nvim-treesitter").install(parsers)
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
+require("telescope").setup({})
+require("telescope").load_extension "fzf"
+require("yazi").setup({
+  open_for_directories = true,
+})
+vim.g.loaded_netrwPlugin = 1
+require("gitsigns").setup({
+  on_attach = function(bufnr)
+    local gs = require("gitsigns")
+    local function map(mode, l, r) vim.keymap.set(mode, l, r, { buffer = bufnr }) end
+    map("n", "]c", function() gs.nav_hunk("next") end)
+    map("n", "[c", function() gs.nav_hunk("prev") end)
+  end,
+})
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
+    end
+  end,
+})
+
+vim.g.gruvbox_material_palette = "mix"
+vim.g.gruvbox_material_transparent_background = 1
+vim.api.nvim_create_autocmd("ColorScheme", {
+  desc = "fix yazi popup border color",
+  callback = function()
+    local fg = vim.api.nvim_get_hl(0, { name = "Normal" }).fg
+    vim.api.nvim_set_hl(0, "FloatBorder", { fg = fg })
+  end,
+})
+vim.cmd.colorscheme("gruvbox-material")
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
+vim.keymap.set("n", "<leader>f", require("telescope.builtin").find_files)
+vim.keymap.set("n", "<leader>e", "<cmd>Yazi<cr>")
